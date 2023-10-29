4f0172bfd6075b2a2a211620c96c2801a3c0532f
Fernandez Francisco Alberto <Franfer0301@gmail.com>
2023-10-29 18:03:43 -0300

    DHT 11

---
+++ main.py
@@ -0,0 +1,29 @@
+import machine
+import time
+import dht
+
+
+class SensorDHT11:
+    def __init__(self, pin):
+        self.sensor = dht.DHT11(pin)
+
+    def leer_datos(self):
+        try:
+            self.sensor.measure()
+            temperature = self.sensor.temperature()
+            humidity = self.sensor.humidity()
+            return temperature, humidity
+        except Exception:
+            return None
+
+pin_dht = 15 
+mi_sensor = SensorDHT11(pin_dht)
+
+while True:
+    data_dht = mi_sensor.leer_datos()
+    if data_dht:
+        temperatura, humedad = data_dht
+        print(f"Temperatura: {temperatura}°C, Humedad: {humedad}%")
+    else:
+        print("Error al leer los datos del sensor")
+    time.sleep(2)
