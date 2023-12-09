ae605933004b75c803e33cef2ba713bdeae9e16c
Fernandez Francisco Alberto <Franfer0301@gmail.com>
2023-12-09 15:19:18 -0300

    new

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-import machine
-import time
-import dht11
+import DHT
 
-Pin_Sensor_DHT = 15
+sensor = dht.DHT11(pin)
+temperature = self.sensor.temperature()
+humidity = self.sensor.humidity()
+return temperature, humidity
 
-Sensor_DHT = SensorDHT11(15)
-
-
-while True:
-    data_dht = Sensor_DHT.read_data()
-    if data_dht:
-        temperatura, humedad = data_dht
-        print(f"Temperatura: {temperatura}°C, Humedad: {humedad}%")
-    else:
-        print("Error al leer los datos del sensor")
-    time.sleep(200)
+if __name__ == '__main__':
+    temperature,humidity = 
